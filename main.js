63074d5a4d59e846ef9154323c2342453618d909
mehwishjbe <mehwish.jbe@gmail.com>
2023-09-21 12:35:58 +0200

    addEventListener on Close hamburger image added

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const hamburgerImage = document.querySelector('.hamburgerImage');
+const hamburgerImageClose = document.querySelector('.hamburgerImageClose');
 
 hamburgerImage.addEventListener('click', function(){
+    const mobileNavbar = document.querySelector('.mobNavbar');
+    mobileNavbar.classList.toggle('active');
+});
+
+hamburgerImageClose.addEventListener('click', function(){
     const mobileNavbar = document.querySelector('.mobNavbar');
     mobileNavbar.classList.toggle('active');
 });
